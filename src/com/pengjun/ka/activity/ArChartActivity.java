68a1c2b5ad594e49c59ef25f7c4fd56e38427978
PenGuin@home <peter517@126.com>
2013-08-12 22:14:31 +0800

    start chart module

--- src/com/pengjun/ka/activity/ArChartActivity.java
+++ src/com/pengjun/ka/activity/ArChartActivity.java
@@ -2,6 +2,7 @@ package com.pengjun.ka.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
@@ -20,7 +21,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.pengjun.ka.component.GalleryFlow;
 import com.pengjun.keepaccounts.R;
@@ -39,8 +39,7 @@ public class ArChartActivity extends Activity {
 		setContentView(R.layout.ar_chart);
 		gfChart = (GalleryFlow) findViewById(R.id.gfChart);
 
-		Integer[] images = { R.drawable.chart_columnar, R.drawable.chart_pie,
-				R.drawable.chart_line };
+		Integer[] images = { R.drawable.chart_columnar, R.drawable.chart_pie, R.drawable.chart_line };
 
 		ImageAdapter adapter = new ImageAdapter(this, images);
 		adapter.createReflectedImages();// 创建倒影效果
@@ -49,37 +48,20 @@ public class ArChartActivity extends Activity {
 		gfChart.setAdapter(adapter);
 
 		gfChart.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				Toast.makeText(ArChartActivity.this, String.valueOf(position),
-						Toast.LENGTH_SHORT).show();
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				if (position == 2) {
+					Intent intent = new Intent(ArChartActivity.this, ArChartDisplayActivity.class);
+					startActivity(intent);
+
+					// Intent intent = new
+					// AverageTemperatureChart().execute(ArChartActivity.this);
+					// startActivity(intent);
+				}
 			}
 
 		});
 		gfChart.setSelection(1);
 
-		// ibBackup.setOnClickListener(new View.OnClickListener() {
-		//
-		// @Override
-		// public void onClick(View v) {
-		// List<AccountRecord> arList = ArService.queryAll();
-		// if (arList.size() != 0) {
-		// FileUtils.backupAr(arList);
-		// }
-		// Util.createAlertDialog(getActivity(), "备份成功").show();
-		// }
-		// });
-		//
-		// ibBackup.setOnLongClickListener(new View.OnLongClickListener() {
-		//
-		// @Override
-		// public boolean onLongClick(View v) {
-		// List<AccountRecord> arList = FileUtils.restoreAr();
-		// ArService.reCreateTable(arList);
-		// Util.createAlertDialog(getActivity(), "还原成功").show();
-		// return true;
-		// }
-		// });
 	}
 
 	class ImageAdapter extends BaseAdapter {
@@ -101,12 +83,11 @@ public class ArChartActivity extends Activity {
 		 */
 		public boolean createReflectedImages() {
 			// 倒影图和原图之间的距离
-			final int reflectionGap = 4;
+			final int reflectionGap = 1;
 			int index = 0;
 			for (int imageId : mImageIds) {
 				// 返回原图解码之后的bitmap对象
-				Bitmap originalImage = BitmapFactory.decodeResource(
-						mContext.getResources(), imageId);
+				Bitmap originalImage = BitmapFactory.decodeResource(mContext.getResources(), imageId);
 				int width = originalImage.getWidth();
 				int height = originalImage.getHeight();
 				// 创建矩阵对象
@@ -116,12 +97,12 @@ public class ArChartActivity extends Activity {
 				matrix.preScale(1, -1);
 
 				// 将矩阵应用到该原图之中，返回一个宽度不变，高度为原图1/2的倒影位图
-				Bitmap reflectionImage = Bitmap.createBitmap(originalImage, 0,
-						height / 2, width, height / 2, matrix, false);
+				Bitmap reflectionImage = Bitmap.createBitmap(originalImage, 0, height / 2, width, height / 2,
+						matrix, false);
 
 				// 创建一个宽度不变，高度为原图+倒影图高度的位图
-				Bitmap bitmapWithReflection = Bitmap.createBitmap(width,
-						(height + height / 2), Config.ARGB_8888);
+				Bitmap bitmapWithReflection = Bitmap.createBitmap(width, (height + height / 2),
+						Config.ARGB_8888);
 
 				// 将上面创建的位图初始化到画布
 				Canvas canvas = new Canvas(bitmapWithReflection);
@@ -131,8 +112,7 @@ public class ArChartActivity extends Activity {
 				deafaultPaint.setAntiAlias(false);
 				// canvas.drawRect(0, height, width, height +
 				// reflectionGap,deafaultPaint);
-				canvas.drawBitmap(reflectionImage, 0, height + reflectionGap,
-						null);
+				canvas.drawBitmap(reflectionImage, 0, height + reflectionGap, null);
 				Paint paint = new Paint();
 				paint.setAntiAlias(false);
 
@@ -140,24 +120,20 @@ public class ArChartActivity extends Activity {
 				 * 参数一:为渐变起初点坐标x位置， 参数二:为y轴位置， 参数三和四:分辨对应渐变终点， 最后参数为平铺方式，
 				 * 这里设置为镜像Gradient是基于Shader类，所以我们通过Paint的setShader方法来设置这个渐变
 				 */
-				LinearGradient shader = new LinearGradient(0,
-						originalImage.getHeight(), 0,
-						bitmapWithReflection.getHeight() + reflectionGap,
-						0x70ffffff, 0x00ffffff, TileMode.MIRROR);
+				LinearGradient shader = new LinearGradient(0, originalImage.getHeight(), 0,
+						bitmapWithReflection.getHeight() + reflectionGap, 0x70ffffff, 0x00ffffff,
+						TileMode.MIRROR);
 				// 设置阴影
 				paint.setShader(shader);
-				paint.setXfermode(new PorterDuffXfermode(
-						android.graphics.PorterDuff.Mode.DST_IN));
+				paint.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.DST_IN));
 				// 用已经定义好的画笔构建一个矩形阴影渐变效果
-				canvas.drawRect(0, height, width,
-						bitmapWithReflection.getHeight() + reflectionGap, paint);
+				canvas.drawRect(0, height, width, bitmapWithReflection.getHeight() + reflectionGap, paint);
 
 				// 创建一个ImageView用来显示已经画好的bitmapWithReflection
 				ImageView imageView = new ImageView(mContext);
 				imageView.setImageBitmap(bitmapWithReflection);
 				// 设置imageView大小 ，也就是最终显示的图片大小
-				imageView
-						.setLayoutParams(new GalleryFlow.LayoutParams(256, 256));
+				imageView.setLayoutParams(new GalleryFlow.LayoutParams(256, 256));
 				// imageView.setScaleType(ScaleType.MATRIX);
 				mImages[index++] = imageView;
 			}
---
+++ src/com/pengjun/ka/activity/ArChartDisplayActivity.java
@@ -0,0 +1,20 @@
+package com.pengjun.ka.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.pengjun.ka.chart.AverageTemperatureChart;
+
+public class ArChartDisplayActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		// WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(new AverageTemperatureChart().getView(this));
+	}
+}
