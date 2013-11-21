0d548c0f76e084dd125b325b251c113d093fab2f
mmp <mmp@mmpc.local>
2013-11-21 20:46:58 +0800

    change to maven project

--- src/com/pengjun/ka/android/fragment/FragmentDirector.java
+++ src/com/pengjun/ka/android/fragment/FragmentDirector.java
@@ -6,11 +6,13 @@ import android.support.v4.app.FragmentTransaction;
 
 public class FragmentDirector {
 
-	public static void replaceFragment(FragmentActivity activity, int layoutId, Fragment fragment) {
-		FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+	public static void replaceFragment(FragmentActivity activity, int layoutId,
+			Fragment fragment) {
+		FragmentTransaction ft = activity.getSupportFragmentManager()
+				.beginTransaction();
 		ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
 		ft.replace(layoutId, fragment);
-		ft.commitAllowingStateLoss();
+		ft.commit();
 	}
 
 }
