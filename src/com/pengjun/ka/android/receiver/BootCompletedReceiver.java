2edef8d3866117cbd43b4bfe87096da5984226e3
unknown <pengjun.pj@ALI-65523W.hz.ali.com>
2014-07-03 20:11:00 +0800

    remove comments

--- src/com/pengjun/ka/android/receiver/BootCompletedReceiver.java
+++ src/com/pengjun/ka/android/receiver/BootCompletedReceiver.java
@@ -1,30 +1,17 @@
 package com.pengjun.ka.android.receiver;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 
 import com.pengjun.ka.android.service.ReportNotificationService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
-	private ReportNotificationService reportService;
-	private ServiceConnection serviceConnection = new ServiceConnection() {
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			reportService = ((ReportNotificationService.ReportServiceBinder) service).getService();
-		}
-
-		public void onServiceDisconnected(ComponentName name) {
-			reportService = null;
-		}
-	};
-
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-			Intent newIntent = new Intent(context, ReportNotificationService.class);
+			Intent newIntent = new Intent(context,
+					ReportNotificationService.class);
 			newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			context.startService(newIntent);
 		}
